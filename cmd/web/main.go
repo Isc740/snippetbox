f6df32c18654f490a40362d6936e0e7567522b33
Isc740 <isaazcantillo@gmail.com>
2024-11-04 13:50:10 -0500

    add fileServer listener

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,15 +6,18 @@ import (
 )
 
 func main() {
-	mux := http.NewServeMux()
+	router := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-	mux.HandleFunc("GET /{$}", homeGet)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetViewGet)
-	mux.HandleFunc("GET /snippet/create", snippetCreateGet)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	router.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	router.HandleFunc("GET /{$}", homeGet)
+	router.HandleFunc("GET /snippet/view/{id}", snippetViewGet)
+	router.HandleFunc("GET /snippet/create", snippetCreateGet)
+	router.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("Starting server on :4000")
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(":4000", router)
 	log.Fatal(err)
 }
