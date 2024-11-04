8c0da96abdc353353601f4b92774d5dd1a5d0c32
Isc740 <isaazcantillo@gmail.com>
2024-11-03 20:27:09 -0500

    add routes

--- main.go
+++ main.go
@@ -9,10 +9,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world!\n"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
 
