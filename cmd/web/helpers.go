2ad8038df9d9aaf87c1cb65c99878429870b5b2b
Isc740 <isaazcantillo@gmail.com>
2024-11-04 19:03:40 -0500

    add helper functions file

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,18 @@ import (
 	"path/filepath"
 )
 
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	method := r.Method
+	uri := r.URL.RequestURI()
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
 type neuteredFileSystem struct {
 	fs http.FileSystem
 }
